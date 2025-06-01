cfd5a87ce03bbd4f32a223bf137b890950f9f10a
Davi Gomes Lucciola <69369398+davi-lucciola@users.noreply.github.com>
2025-06-01 13:37:27 -0300

    Add files via upload

---
+++ Python/Aula09/main.py
@@ -0,0 +1,47 @@
+import streamlit as st
+
+'''
+# Davi Lucciola
+
+![Davi Foto](https://www.github.com/davi-lucciola.png)
+
+Email: davilucciola14@gmail.com
+
+Telefone: (31) 986997442
+
+## Sobre Mim
+
+Desenvolvedor full stack com 2+ anos de experiência especializado
+em desenvolvimento de APIs escaláveis e microserviços.
+
+
+## Habilidades
+
+Python, Flask, FastAPI, Java, Spring Boot, SQL, PostgreSQL 
+React, Next.js, TailwindCSS,Typescript, NodeJS, Didática, Comunicação
+
+## Experiências Profissionais
+
+### Desenvolvedor Backend Pleno - Maxifrota (01/2023 - Hoje)
+
+- Reponsavel Por X
+- Atigi Y Fazendo Isso e Isso e Isso
+- ...
+
+### Outra Experiência
+
+- X
+- Y
+- ...
+
+## Educação
+
+- Análise e Desenvolvimento de Sistemas, Unifavip Wyden (2024 - 2026)
+
+## Linguagens
+
+- Português (Nativo)
+- Inglês (Avançado)
+
+'''
+
